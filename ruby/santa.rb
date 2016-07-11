3d889ac78dd8ab59c11ca075a6d58d58c60794da
Angelo Deleon <rudy209@comcast.net>
2016-07-11 01:48:03 -0700

    Removed method inspect not needed

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,7 +2,7 @@ class Santa
   attr_reader :ethnicity
   attr_accessor :gender, :age
   def initialize(gender, ethnicity)
-    
+
     print "Initializing Santa instance ..."
     @gender = gender
     @ethnicity = ethnicity
@@ -21,16 +21,13 @@ class Santa
   def celebrate_birthday
     @age += 1
   end
-  
+
   def get_mad_at(reindeer)
     @reindeer_ranking.delete reindeer
     puts "You know what you did #{reindeer}.."
     @reindeer_ranking.push reindeer
     puts "To the back Mr.#{reindeer}"
   end
-  
-  def inspect
-    puts 
 
 =begin
   #Setter method 
@@ -48,25 +45,25 @@ class Santa
   end
   
 =end  
-  
+
 end
 
-#DriverCode
-#jim = Santa.new("male", "Latino")
-#puts jim.celebrate_birthday
-#p jim.get_mad_at "Dasher"
-#p jim.gender=("black")
-#p jim.gender
-genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A""black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-santa_num = 0
-while santa_num < 1000
-  santa = Santa.new(genders.sample, ethnicities.sample)
-  santa.age = rand(1..140)
-  puts "Santa # #{santa_num}"
-  puts "Gender: #{santa.gender}"
-  puts "Ethnicity: #{santa.ethnicity}"
-  puts "Age: #{santa.age}"
-  santa_num += 1
-end
+  #DriverCode
+  #jim = Santa.new("male", "Latino")
+  #puts jim.celebrate_birthday
+  #p jim.get_mad_at "Dasher"
+  #p jim.gender=("black")
+  #p jim.gender
+  genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+  ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A""black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+  santa_num = 0
+  while santa_num < 1000
+    santa = Santa.new(genders.sample, ethnicities.sample)
+    santa.age = rand(1..140)
+    puts "Santa # #{santa_num}"
+    puts "Gender: #{santa.gender}"
+    puts "Ethnicity: #{santa.ethnicity}"
+    puts "Age: #{santa.age}"
+    santa_num += 1
+  end
 end
