73755d6213a4cdbf55b639013cbd348b94bd993c
Angelo Deleon <rudy209@comcast.net>
2016-07-18 03:11:18 -0700

    Added new JS functions

---
+++ js/explore.js
@@ -0,0 +1,25 @@
+/*
+- Function that takes a string as a parameter and reverses the string.
+- Takes string as a parameter(ex:Apple) and reverses the string(Apple)
+*/
+
+
+function reverse(word){
+	var string = '';
+	for(var i = word.length - 1; i>=0; i--){
+		string += word[i];
+	}
+	return string;
+}
+
+var reverseAbcd = reverse("basedgod");
+
+	if (1 == 1){
+	 	console.log(reverseAbcd);
+	}
+	else{
+		console.log("abcd")
+	}
+var bob = 1 == 1
+
+console.log(bob)
